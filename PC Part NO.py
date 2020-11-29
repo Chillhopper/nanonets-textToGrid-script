c2c1891c85f8a5c4ba8121e997ab01776a8855a3
Chillhopper <68851163+Chillhopper@users.noreply.github.com>
2020-11-29 14:01:31 +0800

    Update PC Part NO.py
    updated and removed some bugs

--- PC Part NO.py
+++ PC Part NO.py
@@ -1,18 +1,31 @@
 
-def PC_Part_NO_List_1(number_string):
+def PC_Part_NO_List_1(numbers_string):
     numbers = [numbers_string[offset: offset + 12] for offset in range(0, len(numbers_string), 13)]
 
     numbers = []
     for offset in range(0, len(numbers_string), 13):
         numbers.append(numbers_string[offset: offset + 12])
+    for data in numbers:
+        print(data)
 
-def PC_Part_NO_List_2(number_string):
+def PC_Part_NO_List_2(numbers_string):
 
     numbers = [numbers_string[offset : offset + 12] for offset in range(0, len(numbers_string), 13)]
 
     someList = []
     for offset in range(0, len(numbers_string), 11):
         someList.append(numbers_string[offset : offset + 11])
+
+    for data in someList:
+        print(data)
+
+def PC_Part_NO_List_3(numbers_string):
+
+    numbers = [numbers_string[offset : offset + 12] for offset in range(0, len(numbers_string), 14)]
+
+    someList = []
+    for offset in range(0, len(numbers_string), 14):
+        someList.append(numbers_string[offset : offset + 14])
 
     for data in someList:
         print(data)
@@ -26,11 +39,12 @@ def groupFunc(parseData):
     for gil in groupList:
         print(gil)
 
-def figFunc(parseData):
+def figitemFunc(parseData):
     groupList = []
     for spacing in range(0, len(parseData), 4):
         groupList.append(parseData[spacing : spacing + 3])
 
     for gil in groupList:
         print("\'" + gil)
+
 
